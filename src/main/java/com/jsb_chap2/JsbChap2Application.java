79edf222dbf59ac6395f56083fc55def31f53fb5
77r77r <codingworld@naver.com>
2025-09-09 00:31:47 +0900

    init

---
+++ src/main/java/com/jsb_chap2/JsbChap2Application.java
@@ -0,0 +1,13 @@
+package com.jsb_chap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbChap2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsbChap2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsb_chap2/JsbChap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsb_chap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsbChap2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
